727464383511cb151b1646d08a862f8955a2e7cb
iSong <i_songwei@163.com>
2017-04-11 13:49:14 +0800

    UIMotionEffect

--- UIMotionEffect/ViewController.m
+++ UIMotionEffect/ViewController.m
@@ -16,14 +16,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    /*
+     @property (readonly, nonatomic) NSString *keyPath;
+     @property (readonly, nonatomic) UIInterpolatingMotionEffectType type;
+     
+     @property (nullable, strong, nonatomic) id minimumRelativeValue;
+     @property (nullable, strong, nonatomic) id maximumRelativeValue;
+     */
+    
+    /*
+     keyPath，左右翻转屏幕将要影响到的属性，比如center.x。
+     type（UIInterpolatingMotionEffectType类型），观察者视角，也就是屏幕倾斜的方式，目前区分水平和垂直两种方式。
+     minimumRelativeValue和maximumRelativeValue，keyPath对应的值的变化范围，注意这个是id类型。min对应最小的offset，max对应最大的offset。
+     */
+    
+    for (UIMotionEffect *effect in self.view.motionEffects)
+    {
+        //已经存在效果则删除效果
+        [self.view removeMotionEffect:effect];
+    }
+    UIInterpolatingMotionEffect * xEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.x" type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+    xEffect.minimumRelativeValue =  [NSNumber numberWithFloat:-100.0];
+    xEffect.maximumRelativeValue = [NSNumber numberWithFloat:100.0];
+    
+    
+    UIInterpolatingMotionEffect * yEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.y" type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
+    yEffect.minimumRelativeValue =  [NSNumber numberWithFloat:-100.0];
+    yEffect.maximumRelativeValue = [NSNumber numberWithFloat:100.0];
+    
+    [self.view addMotionEffect:xEffect];
+    [self.view addMotionEffect:yEffect];
+    
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
