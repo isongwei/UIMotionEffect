992e5ef73fe33706522e86bd4bb0444d1f5ca2fe
iSong <i_songwei@163.com>
2017-04-11 13:34:26 +0800

    Initial Commit

---
+++ UIMotionEffect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIMotionEffect
+//
+//  Created by iSongWei on 2017/4/11.
+//  Copyright © 2017年 iSong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIMotionEffect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIMotionEffect
+//
+//  Created by iSongWei on 2017/4/11.
+//  Copyright © 2017年 iSong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
